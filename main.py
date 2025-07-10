84dad5035f8946550baba05f144bc393593df202
jainerorama <jocarralero@gmail.com>
2025-07-10 22:15:42 +0200

    adding functions to sort the count of characters

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_num_words 
 from stats import get_num_characters
+from stats import get_report 
 
 def get_book_text(filepath):
         with open(filepath) as f:
@@ -7,8 +8,17 @@ def get_book_text(filepath):
 
 def main():
         book_text = get_book_text("./books/frankenstein.txt")
+        print("----------- Word Count ----------")
         get_num_words(book_text) 
-        get_num_characters(book_text)
+        dic = get_num_characters(book_text)
+        print("--------- Character Count -------")
+        for r in get_report(dic):
+            c = r["char"].isalpha()
+            count = r["num"]
+            if c:
+                print(f"{r['char']}: {count}")
+        print("========= END ==========")
+
 
 main()
 
--- stats.py
+++ stats.py
@@ -1,7 +1,7 @@
 def get_num_words(text):
         words = text.split()
         words_count = len(words)
-        print(f"{words_count} words found in the document")
+        print(f"Found {words_count} total words")
 
 
 def get_num_characters(text):
@@ -9,4 +9,18 @@ def get_num_characters(text):
         num_characters = {}
         for character in words:
                 num_characters[character.lower()] = num_characters.get(character.lower(), 0) + 1
-        print(f"{num_characters} this i the count")
+        return num_characters
+
+def sort_on(items):
+        return items["num"]
+
+def get_report(dic):
+        dic_list = []
+        for d in dic:
+            new_dic = {}
+            count = dic[d]
+            new_dic["char"] = d
+            new_dic["num"] = count
+            dic_list += [new_dic]
+        dic_list.sort(reverse=True, key=sort_on)
+        return dic_list
