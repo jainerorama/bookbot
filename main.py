65baf6fffb43441ab4a8e38ecec3b3be1b51eeea
jainerorama <jocarralero@gmail.com>
2025-07-12 07:30:16 +0200

    Updating main.py get book function to be able to write in the CLI the book that we want to analyze

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+import sys
 from stats import get_num_words 
 from stats import get_num_characters
 from stats import get_report 
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 
 def get_book_text(filepath):
         with open(filepath) as f:
              return f.read()
 
 def main():
-        book_text = get_book_text("./books/frankenstein.txt")
+        book_text = get_book_text(sys.argv[1])
         print("----------- Word Count ----------")
         get_num_words(book_text) 
         dic = get_num_characters(book_text)
